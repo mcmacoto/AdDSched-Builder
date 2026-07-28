fed78c5ea4e3117609f015db09ce0c5e9603d992
mcmacoto <150632672+mcmacoto@users.noreply.github.com>
2026-07-28 14:18:12 +0800

    Fixed issues for deployment

--- addsched/settings.py
+++ addsched/settings.py
@@ -65,7 +65,7 @@ USE_TZ = False
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 STORAGES = {
     "staticfiles": {
--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --clear
+python3 manage.py collectstatic --no-input --clear
