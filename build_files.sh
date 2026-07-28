9215a37cb5796beb8f26e2ae1d1ff2dea296f497
mcmacoto <150632672+mcmacoto@users.noreply.github.com>
2026-07-28 14:19:37 +0800

    Updated build build_files

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
-python3 manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear
